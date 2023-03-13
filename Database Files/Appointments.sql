485f967d1ce86ebb58e144ba9bae3d2ea1e4fd42
Deepthi Chadalavada Pfw <122562387+deepthi-912@users.noreply.github.com>
2023-03-13 14:10:14 -0400

    Update Appointments sql with more rows in the table
    Update Appointments sql with more rows in the table

--- Database Files/Appointments.sql
+++ Database Files/Appointments.sql
@@ -1,12 +1,9 @@
-//Database for ccreating Doctor Appointments 
 create database Appointments;
-select doctor_id from Appointments.Appointments;
 
-//Database Table Creation 
-CREATE TABLE  Appointments.Appointments(
-  doctor_id INT NOT NULL,
+CREATE TABLE Appointments.Appointments (
+  doctor_id INT NOT NULL PRIMARY KEY,
   patient_id INT NOT NULL,
-  appointment_id INT NOT NULL PRIMARY KEY ,
+  appointment_id INT NOT NULL,
   appointment_time DATETIME NOT NULL,
   patient_name VARCHAR(255) NOT NULL,
   doctor_name VARCHAR(255) NOT NULL,
@@ -15,20 +12,34 @@ CREATE TABLE  Appointments.Appointments(
   patient_age INT NOT NULL
 );
 
-//Insertion of values in database system 
-INSERT INTO Appointments.Appointments(doctor_id, patient_id, appointment_id, appointment_time, patient_name, doctor_name, doctor_department, patient_disease, patient_age) VALUES
-(1, 1, 1, '2023-03-05 09:00:00', 'John Doe', 'Dr. Smith', 'Cardiology', 'Heart Attack', 56),
+
+INSERT INTO Appointments.Appointments (doctor_id, patient_id, appointment_id, appointment_time, patient_name, doctor_name, doctor_department, patient_disease, patient_age) VALUES
+(1, 1, 1, '2023-03-05 09:00:00', 'John Doe', 'Dr. Maya', 'Cardiology', 'Heart Attack', 56),
 (2, 2, 2, '2023-03-06 10:00:00', 'Jane Doe', 'Dr. Lee', 'Oncology', 'Breast Cancer', 42),
 (3, 3, 3, '2023-03-07 11:00:00', 'Bob Johnson', 'Dr. Wilson', 'Orthopedics', 'Fractured Arm', 35),
-(4, 4, 4, '2023-03-08 12:00:00', 'Alice Johnson', 'Dr. Jackson', 'Neurology', 'Migraine', 28),
-(5, 5, 5, '2023-03-09 13:00:00', 'Sarah Lee', 'Dr. Neilson', 'Allergy and Immunology', 'Allergies', 19),
-(1, 6, 6, '2023-03-10 14:00:00', 'Tom Davis', 'Dr. Smith', 'Cardiology', 'Asthma', 49),
-(6, 7, 7, '2023-03-11 15:00:00', 'Linda Wilson', 'Dr. Carolina', 'Dermatology', 'Psoriasis', 61),
-(7, 8, 8, '2023-03-12 16:00:00', 'David Brown', 'Dr. Katie', 'Infectious Disease', 'Flu', 32),
-(8, 9, 9, '2023-03-13 17:00:00', 'Samantha Green', 'Dr. Charles', 'Gastroenterology', 'Irritable Bowel Syndrome', 47),
-(10, 10, 10, '2023-03-14 18:00:00', 'Peter White', 'Dr. Monica', 'Endocrinology', 'Diabetes', 52),
-(11, 11, 11, '2023-03-15 19:00:00', 'Karen Lewis', 'Dr. Deepthi', 'Rheumatology', 'Arthritis', 71),
-(3, 12, 12, '2023-03-16 20:00:00', 'Mark Roberts', 'Dr. Jackson', 'Neurology', 'Parkinson Disease', 59),
-(12, 13, 13, '2023-03-17 09:00:00', 'Catherine Johnson', 'Dr. Smitha', 'Psychiatry', 'Depression', 43),
-(2, 14, 14, '2023-03-18 10:00:00', 'Alexandra Lee', 'Dr. Lee', 'Oncology', 'Lung Cancer', 34),
-(5, 15, 15, '2023-03-19 11:00:00', 'Laura Davis', 'Dr. Neilson', 'Allergy and Immunology', 'Hay Fever', 51);
+(4, 4, 4, '2023-03-08 12:00:00', 'Alice Johnson', 'Dr. Smith', 'Neurology', 'Migraine', 28),
+(5, 5, 5, '2023-03-09 13:00:00', 'Sarah Lee', 'Dr. Deepthi', 'Allergy and Immunology', 'Allergies', 19),
+(6, 6, 6, '2023-03-10 14:00:00', 'Tom Davis', 'Dr. Dustin', 'Pulmonology', 'Asthma', 49),
+(7, 7, 7, '2023-03-11 15:00:00', 'Linda Wilson', 'Dr. Jack', 'Dermatology', 'Psoriasis', 61),
+(8, 8, 8, '2023-01-12 16:00:00', 'David Brown', 'Dr. Gourav', 'Infectious Disease', 'Flu', 32),
+(9, 9, 9, '2023-02-13 17:00:00', 'Samantha Green', 'Dr. Charles', 'Gastroenterology', 'Irritable Bowel Syndrome', 47),
+(10, 10, 10, '2023-01-14 18:00:00', 'Peter White', 'Dr. Joule', 'Endocrinology', 'Diabetes', 52),
+(7, 11, 11, '2023-02-15 19:00:00', 'Karen Lewis', 'Dr. Jack', 'Dermatology', 'Tooth Pain', 71),
+(4, 12, 12, '2023-02-16 20:00:00', 'Mark Roberts', 'Dr. Smith', 'Neurology', 'Parkinson Disease', 59),
+(11, 13, 13, '2023-02-17 09:00:00', 'Catherine Johnson', 'Dr.Carolin', 'Psychiatry', 'Depression', 43),
+(2, 14, 14, '2023-02-19 10:00:00', 'Alexandra Lee', 'Dr. Lee', 'Oncology', 'Lung Cancer', 34),
+(5, 15, 15, '2023-01-28 11:00:00', 'Laura Davis', 'Dr. Deepthi', 'Allergy and Immunology', 'Hay Fever', 51);
+
+INSERT INTO Appointments.Appointments (doctor_id, patient_id, appointment_id, appointment_time, patient_name, doctor_name, doctor_department, patient_disease, patient_age) VALUES
+(12, 16, 16, '2023-01-12 11:00:00', 'Sierra', 'Dr. David', 'Allergy and Immunology', 'Hay Fever', 51),
+(3, 3, 17, '2023-01-25 11:00:00', 'Bob Johnson', 'Dr. Wilson', 'Orthopedics', 'Fractured Arm', 35),
+(6, 6, 18, '2023-02-17 12:00:00', 'Tom Davis', 'Dr. Wilson', 'Pulmonology', 'Pregnancy Test', 30),
+(13, 17, 19, '2023-02-22 1:00:00', 'Warner', 'Dr. Prem', 'Allergy and Immunology', 'Hay Fever', 51),
+(8, 15, 20, '2023-03-08 2:00:00', 'Laura Davis', 'Dr. Gourav', 'Allergy and Immunology', 'Hay Fever', 51),
+(1, 1, 21, '2023-02-06 3:00:00', 'John Doe', 'Dr. Maya', 'Cardiology', 'Heart Attack', 56),
+(14, 17, 22, '2023-03-09 4:00:00', 'Laura Davis', 'Dr. Deepthi', 'Allergy and Immunology', 'Hay Fever', 51),
+(15, 18, 23, '2023-02-25 6:00:00', 'Shruthi', 'Dr. Zim', 'Pulmonology', 'Hay Fever', 45),
+(7, 11, 24, '2022-12-12 7:00:00', 'Karen Lewis', 'Dr. Jack', 'Dermatology', 'Tooth Pain', 71),
+(8, 8, 25, '2021-02-1 8:00:00', 'David Brown', 'Dr. Gourav', 'Infectious Disease', 'Flu', 32);
+
+select * from Appointments.Appointments;
