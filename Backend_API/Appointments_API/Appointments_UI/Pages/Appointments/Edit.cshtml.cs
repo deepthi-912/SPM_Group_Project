fa6866218d123c0ccdd1bbd109d86ae6ec8361c4
ShrutiMahishi24-pfw <122409163+ShrutiMahishi24-pfw@users.noreply.github.com>
2023-03-23 16:03:10 -0400

    Update Edit.cshtml.cs

--- Backend_API/Appointments_API/Appointments_UI/Pages/Appointments/Edit.cshtml.cs
+++ Backend_API/Appointments_API/Appointments_UI/Pages/Appointments/Edit.cshtml.cs
@@ -1,67 +1,75 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using HWK4.Models;
+using Appointments_API.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text.Json;
 using Newtonsoft.Json;
 using HWK6.Pages;
+using Appointments_API;
 
-namespace HWK6.Pages.Expenditures
+namespace Appointments_UI.Pages.Appointments
 {
-    using HWK4.Models;
+    using Appointments_API.Models;
 
     //Edits the item values
     public class EditModel : PageModel
     {
-        public Expenditures todo = new();
+        public Appointments todo = new();
         public string errorMessage = "";
         public string successMessage = "";
 
         public async void OnGet()
         {
-            string id = Request.Query["id"];
+            string id = Request.Query["appointment_id"];
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:5273");
+                client.BaseAddress = new Uri("http://localhost:5053");
                 //HTTP GET
-                var responseTask = client.GetAsync("Expenditures/" + id);
+                var responseTask = client.GetAsync("Appointments/" + id);
                 responseTask.Wait();
 
                 var result = responseTask.Result;
                 if (result.IsSuccessStatusCode)
                 {
                     var readTask = await result.Content.ReadAsStringAsync();
-                    todo = JsonConvert.DeserializeObject<Expenditures>(readTask);
+                    todo = JsonConvert.DeserializeObject<Appointments>(readTask);
                 }
             }
         }
 
         public async void OnPost()
         {
-            todo.ID = int.Parse(Request.Form["ID"]);
-            todo.ExpenditureType = Request.Form["ExpenditureType"];
 
-            todo.Expenditure = int.Parse(Request.Form["Expenditure"]);
+            todo.appointment_id = int.Parse(Request.Form["appointment_id"]);
+            todo.doctor_id = int.Parse(Request.Form["appointment_id"]);
+            todo.patient_id = int.Parse(Request.Form["appointment_id"]);
+            todo.appointment_time = DateTime.Parse(Request.Form["appointment_time"]);
+            todo.patient_name = Request.Form["patient_name"];
+            todo.doctor_name = Request.Form["doctor_name"];
+            todo.doctor_department = Request.Form["doctor_department"];
+            todo.patient_disease = Request.Form["patient_disease"];
 
-            if (todo.ExpenditureType.Length == 0 || todo.Expenditure==0)
+            todo.patient_age = int.Parse(Request.Form["patient_age"]);
+
+            if (todo.appointment_time == null)
             {
-                errorMessage = "Expenditure Type is Required";
+                errorMessage = "Appointment Time is Required";
             }
             else
             {
                 var opt = new JsonSerializerOptions() { WriteIndented = true };
-                string json = System.Text.Json.JsonSerializer.Serialize<Expenditures>(todo, opt);
+                string json = System.Text.Json.JsonSerializer.Serialize<Appointments>(todo, opt);
 
                 using (var client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("http://localhost:5273");
+                    client.BaseAddress = new Uri("http://localhost:5053");
 
                     var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
 
-                    var result = await client.PutAsync("Expenditures", content);
+                    var result = await client.PutAsync("Appointments", content);
                     string resultContent = await result.Content.ReadAsStringAsync();
                     Console.WriteLine(resultContent);
 
