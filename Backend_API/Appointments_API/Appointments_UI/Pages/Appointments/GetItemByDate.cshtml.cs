c9bc87fd8ff4adbf7bd989f071b97c15411a6252
premravva-PFW <122707632+premravva-PFW@users.noreply.github.com>
2023-04-03 17:03:49 -0400

    Update GetItemByDate.cshtml.cs

--- Backend_API/Appointments_API/Appointments_UI/Pages/Appointments/GetItemByDate.cshtml.cs
+++ Backend_API/Appointments_API/Appointments_UI/Pages/Appointments/GetItemByDate.cshtml.cs
@@ -10,7 +10,10 @@ using Newtonsoft.Json;
 namespace Appointments_UI.Pages.Appointments
 {
     using Appointments_API.Models;
-    /// Gets the item inputs from the UI, evaluates them on the basis of appointment date and displays the details
+    
+    ///<summary>
+      /// Gets the item inputs from the UI, evaluates them on the basis of appointment date and displays the details
+    ///</summary>
     public class GetItemByDateModel : PageModel
     {
         public List<Appointment> ap = new();
@@ -22,7 +25,10 @@ namespace Appointments_UI.Pages.Appointments
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("http://localhost:5053");
-                ///HTTP GET to obtain the appointment details based on the appointment date.
+               
+                ///<summary>
+                    ///HTTP GET to obtain the appointment details based on the appointment date.
+                ///</summary>
                 var responseTask = client.GetAsync("Appointment/Analysis-GetAppointmentsByDate?dt=" + id);
                 responseTask.Wait();
                 var result = responseTask.Result;
