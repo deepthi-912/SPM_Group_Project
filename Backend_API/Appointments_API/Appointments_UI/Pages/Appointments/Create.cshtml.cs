6c06a051e8128431dcfde288340bba774a2869a7
Deepthi Chadalavada Pfw <122562387+deepthi-912@users.noreply.github.com>
2023-04-02 23:19:48 -0400

    Update Create file for UI
    Update Create a file for UI

--- Backend_API/Appointments_API/Appointments_UI/Pages/Appointments/Create.cshtml.cs
+++ Backend_API/Appointments_API/Appointments_UI/Pages/Appointments/Create.cshtml.cs
@@ -11,15 +11,15 @@ namespace Appointments_UI.Pages.Appointments
     using Appointments_API.Models;
     public class CreateModel : PageModel
     {
-        public Appointments todo = new();
+        public Appointment todo = new();
         public string errorMessage = "";
         public string successMessage = "";
         public async void OnPost()
         {
 
             todo.appointment_id = int.Parse(Request.Form["appointment_id"]);
-            todo.doctor_id = int.Parse(Request.Form["appointment_id"]);
-            todo.patient_id = int.Parse(Request.Form["appointment_id"]);
+            todo.doctor_id = int.Parse(Request.Form["doctor_id"]);
+            todo.patient_id = int.Parse(Request.Form["patient_id"]);
             todo.appointment_time = DateTime.Parse(Request.Form["appointment_time"]);
             todo.patient_name = Request.Form["patient_name"];
             todo.doctor_name = Request.Form["doctor_name"];
@@ -35,12 +35,12 @@ namespace Appointments_UI.Pages.Appointments
             else
             {
                 var opt = new JsonSerializerOptions() { WriteIndented = true };
-                string json = System.Text.Json.JsonSerializer.Serialize<Appointments>(todo, opt);
+                string json = System.Text.Json.JsonSerializer.Serialize<Appointment>(todo, opt);
                 using (var client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("http://localhost:5071");
+                    client.BaseAddress = new Uri("http://localhost:5053");
                     var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-                    var result = await client.PostAsync("Appointments", content);
+                    var result = await client.PostAsync("Appointment", content);
                     string resultContent = await result.Content.ReadAsStringAsync();
                     Console.WriteLine(resultContent);
                     if (!result.IsSuccessStatusCode)
